9e08f8036d356881b76d121b1d7f688838d29926
JorqueraFrancisco <33900932+JorqueraFrancisco@users.noreply.github.com>
2018-01-03 15:59:11 -0300

    ultimo comit

--- Program.cs
+++ Program.cs
@@ -12,16 +12,13 @@ namespace Cotizaciones
 {
     public class Program
     {
-        public Program (){
-
-        }
-        public  void Correr()
+        public static void Main(string[] args)
         {
-            BuildWebHost().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost() =>
-            WebHost.CreateDefaultBuilder()
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
     }
